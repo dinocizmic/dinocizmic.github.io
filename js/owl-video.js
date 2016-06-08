5b3c7fb293180be1a3d306e9e1ad31ada09fab21
dinocizmic <dinocizmic@me.com>
2016-06-07 20:43:33 -0700

    Carousel
    Changed youtube links, changed the js to include 1 and 2 for 480
    instead of 600.

--- js/owl-video.js
+++ js/owl-video.js
@@ -3,16 +3,16 @@ $('.owl-carousel').owlCarousel({
         items:1,
         merge:true,
         loop:true,
-        margin:10,
+        
         video:true,
         lazyLoad:true,
         center:true,
         responsive:{
             480:{
-                items:2
+                items:1
             },
             600:{
-                items:4
+                items:2
             }
         }
     });
